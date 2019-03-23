9412603db4faea6270cb5bd17ae3e95bf1877055
andreivercosa <andreivercosa@gmail.com>
2019-03-22 21:14:35 -0400

    v1

---
+++ ListaMunicipioApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xamarin.Forms;
+
+namespace ListaMunicipioApp
+{
+    public partial class MainPage : ContentPage
+    {
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            string UF = textUF.Text.ToUpper();
+            var client = new HttpClient();
+            var json = await client.GetStringAsync($"http://ibge.herokuapp.com/municipio/?val={UF}");
+            var dados = JsonConvert.DeserializeObject<Object>(json);
+
+            JObject municipios = JObject.Parse(json);
+
+            Dictionary<string, string> dadosMunicipios = municipios.ToObject<Dictionary <string, string>>();
+
+            ArrayList lista = new ArrayList();
+            foreach (KeyValuePair<string, string> municipio in dadosMunicipios)
+            {
+                lista.Add(municipio.Key);
+            }
+            ListaMunicipio.ItemsSource = lista;
+
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
